931bef81cb8a59f58bec60e7adb54ddf8daa1b72
Vinny021 <viniciusbarbosa021@gmail.com>
2022-05-29 10:51:53 -0300

    Verificação de idade + Mensagem nova

--- f1.py
+++ f1.py
@@ -1,1 +1,8 @@
-print("Hello World")
+print("Bem vindom Aguarda por novidades....")
+
+idade = int(input("Digite sua ideda: "))
+
+if( idade > 18):
+    print("Você é menor de idade")
+else:
+    print("Pode entrar")
